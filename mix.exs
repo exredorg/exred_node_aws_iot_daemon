a3f318fc3a0d1c21ee83568db86cf07b90f3f9cf
Zsolt Keszthelyi <zsolt.erl@gmail.com>
2018-03-16 21:27:06 -0400

    fixed deps in mix file

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Exred.Node.AwsIotDaemon.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:exred_library, path: "../exred_library"},
+      # {:exred_library, path: "../exred_library"},
+      {:exred_library, git: "git@bitbucket.org:zsolt001/exred_library.git", app: false}
       {:gen_mqtt, "~> 0.4.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
