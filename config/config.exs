1b2d245f3156133aaaa6e919485d278cf5f9fafc
Zsolt Keszthelyi <zsolt.erl@gmail.com>
2018-06-10 17:39:15 -0400

    added conform to handle configuration

--- config/config.exs
+++ config/config.exs
@@ -4,8 +4,8 @@ use Mix.Config
 # these get passed to the Erlang ssl module
 # see ssl_option() here: http://erlang.org/doc/man/ssl.html
 config :exred_node_aws_iot_daemon, :ssl,
-  keyfile: "/Users/zkeszthelyi/src/exred/certs/ff925dd2f1-private.pem.key",
-  certfile: "/Users/zkeszthelyi/src/exred/certs/ff925dd2f1-certificate.pem.crt",
-  cacertfile: "/Users/zkeszthelyi/src/exred/certs/symantec_ca_root.pem"
+  keyfile: "~/exred_data/private.pem.key",
+  certfile: "~/exred_data/certificate.pem.crt",
+  cacertfile: "~/exred_data/ca_root.pem"
 
 # import_config "#{Mix.env}.exs"
